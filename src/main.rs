e1403eeac93556b061340f6ab51535f038ab8ecf
jedkutai <jedkutai@gmail.com>
2026-06-22 14:07:02 -0400

    lights burn dimmer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+use rppal::gpio::Gpio;
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
+
+const LED_PIN: u8 = 17;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Starting LED blink test...");
+
+    let gpio = Gpio::new()?;
+    let mut led = gpio.get(LED_PIN)?.into_output();
+
+    loop {
+        led.set_high();
+        println!("LED on");
+        sleep(Duration::from_millis(500));
+
+        led.set_low();
+        println!("LED off");
+        sleep(Duration::from_millis(500));
+    }
 }
